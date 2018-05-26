e09325dec3e93baa3c890900e88bbe0ae5c84fd4
JaneLawson <jane_lawson04@hotmail.com>
2018-05-26 06:47:39 +0100

    added tests around expense form simulating user input

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,57 @@ test('should render expense form with expense data', () => {
     const wrapper = shallow(<ExpenseForm  expense={expenses[0]}/>);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should render error for invalid test submission', () => {
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => { } // fake the things on the args that get used. So e is the arg, but prevent default is whats used
+    });
+    expect(wrapper.state('error')).toEqual('Please provide an amount and description');
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on input change', () => {
+    const value = 'new description';
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('input').at(0).simulate('change', { // at(0) means the first input as there are a few
+        target:  { value }  //target.value is what we need to access
+     });
+     expect(wrapper.state('description')).toBe(value);
+     expect(wrapper).toMatchSnapshot();
+});
+
+test('should set note on input change', () => {
+    const value = 'new note';
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('textarea').simulate('change', {   // at(0) means the first input as there are a few
+        target:  { value }      //target.value is what we need to access
+     });
+     expect(wrapper.state('note')).toBe(value);
+     expect(wrapper).toMatchSnapshot();
+});
+
+test('should set valid amount on input change', () => {
+    const value = '10.00';
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('input').at(1).simulate('change', {   
+        target:  { value }      
+     });
+     expect(wrapper.state('amount')).toBe(value);
+     expect(wrapper).toMatchSnapshot();
+});
+
+test('should not set invalid amount on input change', () => {
+    const value = '51.333';
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('input').at(1).simulate('change', {   
+        target:  { value }      
+     });
+     expect(wrapper.state('amount')).toEqual('');
+     expect(wrapper).toMatchSnapshot();
+});
