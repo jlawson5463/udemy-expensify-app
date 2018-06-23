12264938ca8c93b30840f69643a6a920731a7406
JaneLawson <jane_lawson04@hotmail.com>
2018-06-23 14:53:03 +0100

    added an authentication reducer to track log in state with store

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import { startLogout } from '../actions/auth';
 import  { connect } from 'react-redux';
 
 export const Header = ({ startLogout }) => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Login</NavLink>
-        <NavLink to="/Dashboard" activeClassName="is-active">Dashboard</NavLink>
+        <NavLink to="/" activeClassName="is-active" exact={true}></NavLink>
+        <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
         <button onClick={startLogout}>Logout</button>
     </header>
@@ -17,4 +17,4 @@ const mapDispatchToProps = (dispatch) => ({
     startLogout: () => dispatch(startLogout())
 })
 
-export default connect(undefined, mapDispatchToProps)(Header);
+export default withRouter(connect(undefined, mapDispatchToProps)(Header));
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { Router, Route, Switch, Link } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
@@ -8,10 +9,12 @@ import Header from '../components/Header';
 import NotFound from '../components/NotFound';
 import LoginPage from '../components/LoginPage';
 
+export const history = createHistory();
+
 const AppRouter = () => (
-    <BrowserRouter>
+    <Router history={history}>
     <div>
-        <Header />
+        <Header/>
         <Switch>
             <Route path="/" component={LoginPage} exact={true} />
             <Route path="/dashboard" component={ExpenseDashboardPage} />
@@ -22,7 +25,7 @@ const AppRouter = () => (
             <Route component={NotFound} />
         </Switch>
         </div>
-    </BrowserRouter>
+    </Router>
 );
 
 export default AppRouter;
