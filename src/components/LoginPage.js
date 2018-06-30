92b2e11f93ba0583d4d3e40c0f73d23c2a3f3ef6
JaneLawson <jane_lawson04@hotmail.com>
2018-06-30 07:14:53 +0100

    changed login text

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,17 +3,21 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-    <div className="box-layout">
-       <div className="box-layout__box">
-            <h1 className="box-layout__title">Clever name inserted here</h1> 
-            <p>It's time to get your expenses under control.</p>
-            <button onClick={startLogin} className="button">Login with Google</button>
-       </div>
-    </div>
-)
+	<div className="box-layout">
+		<div className="box-layout__box">
+			<h1 className="box-layout__title">Manage your expenses</h1>
+			<button onClick={startLogin} className="button">
+				Login with Google
+			</button>
+		</div>
+	</div>
+);
 
-const mapDispatchToProps = (dispatch) => ({
-    startLogin: () => dispatch(startLogin())
+const mapDispatchToProps = dispatch => ({
+	startLogin: () => dispatch(startLogin())
 });
 
-export default connect(undefined, mapDispatchToProps)(LoginPage);
+export default connect(
+	undefined,
+	mapDispatchToProps
+)(LoginPage);
